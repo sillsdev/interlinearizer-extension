178bad62aff043239c62020749988dfb22bc76d0
Danny Rorabaugh <imnasnainaec@gmail.com>
2026-05-21 14:48:09 -0400

    Format

--- src/components/ProjectModals.tsx
+++ src/components/ProjectModals.tsx
@@ -2,7 +2,10 @@ import type { UseWebViewStateHook } from '@papi/core';
 import { useCallback, useState } from 'react';
 import { CreateProjectModal } from './CreateProjectModal';
 import { ProjectMetadataModal } from './ProjectMetadataModal';
-import type { ActiveProjectState, InterlinearProjectSummary } from '../types/interlinear-project-summary';
+import type {
+  ActiveProjectState,
+  InterlinearProjectSummary,
+} from '../types/interlinear-project-summary';
 import { SelectInterlinearProjectModal } from './SelectInterlinearProjectModal';
 
 /** Which modal is currently visible. Only one can be open at a time. */
